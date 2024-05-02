575a51eafa93d750dd10d99ae578b94e58388271
Aashish129 <38881348+Aashish129@users.noreply.github.com>
2024-05-02 14:57:24 +0530

    Add files via upload
    This Projects takes user input name and also a pet name . Provides your favorite band name

---
+++ bandname_generator.py
@@ -0,0 +1,5 @@
+user_name = input("Hi!, What is your name ?\n")
+
+print("Hi " + user_name + "!")
+pet_name = input("What is your pet name?\n")
+print("Your band name is " + user_name + " " + pet_name + "\nHave a nice day!")Aa
