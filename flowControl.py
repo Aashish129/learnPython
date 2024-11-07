688853163a38e467a9bf430352636f572f1c2da4
Aashish129 <aashish3024@gmail.com>
2024-11-07 14:34:29 +0530

    Initial Version: A Basic Flow Control Program

---
+++ flowControl.py
@@ -0,0 +1,10 @@
+name = input("Please Enter Your First Name: ")
+
+if name == "Aashish":
+    print(f'Hello {name}')
+
+elif name == "Richa":
+    print(f'Hello {name}')
+
+else:
+    print(f'Hello Stranger!')
